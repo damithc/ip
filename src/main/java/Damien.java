2cc9f6276ac2b81eee0cd5c9da2255aee2dbd89f
damithc <1673303+damithc@users.noreply.github.com>
2026-08-02 01:10:29 +0800

    Implement A-Classes

--- src/main/java/Damien.java
+++ src/main/java/Damien.java
@@ -6,8 +6,7 @@ public class Damien {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] taskDescriptions = new String[MAX_TASKS];
-        boolean[] taskDone = new boolean[MAX_TASKS];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
 
         System.out.println(LINE);
@@ -28,24 +27,24 @@ public class Damien {
             if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + "." + formatTask(taskDescriptions[i], taskDone[i]));
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             } else if (command.startsWith("mark ")) {
                 int taskIndex = getTaskIndex(command, "mark ");
                 if (isValidTaskIndex(taskIndex, taskCount)) {
-                    taskDone[taskIndex] = true;
+                    tasks[taskIndex].markAsDone();
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + formatTask(taskDescriptions[taskIndex], taskDone[taskIndex]));
+                    System.out.println("  " + tasks[taskIndex]);
                 }
             } else if (command.startsWith("unmark ")) {
                 int taskIndex = getTaskIndex(command, "unmark ");
                 if (isValidTaskIndex(taskIndex, taskCount)) {
-                    taskDone[taskIndex] = false;
+                    tasks[taskIndex].unmark();
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  " + formatTask(taskDescriptions[taskIndex], taskDone[taskIndex]));
+                    System.out.println("  " + tasks[taskIndex]);
                 }
             } else {
-                taskDescriptions[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println("added: " + command);
             }
@@ -64,10 +63,5 @@ public class Damien {
 
     private static boolean isValidTaskIndex(int taskIndex, int taskCount) {
         return taskIndex >= 0 && taskIndex < taskCount;
-    }
-
-    private static String formatTask(String description, boolean isDone) {
-        String status = isDone ? "[X]" : "[ ]";
-        return status + " " + description;
     }
 }

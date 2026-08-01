9215b4281eae30072b79c5160b93344e70c765d3
damithc <1673303+damithc@users.noreply.github.com>
2026-08-02 00:07:50 +0800

    Add Level 1 command echoing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(LINE);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(LINE);
+        }
     }
 }

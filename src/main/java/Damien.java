631f6a43546cdb0b081f83d80969331c33823408
damithc <1673303+damithc@users.noreply.github.com>
2026-08-02 00:50:06 +0800

    Implement Level-3

--- src/main/java/Damien.java
+++ src/main/java/Damien.java
@@ -6,7 +6,8 @@ public class Damien {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[MAX_TASKS];
+        String[] taskDescriptions = new String[MAX_TASKS];
+        boolean[] taskDone = new boolean[MAX_TASKS];
         int taskCount = 0;
 
         System.out.println(LINE);
@@ -25,16 +26,48 @@ public class Damien {
             }
 
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + formatTask(taskDescriptions[i], taskDone[i]));
+                }
+            } else if (command.startsWith("mark ")) {
+                int taskIndex = getTaskIndex(command, "mark ");
+                if (isValidTaskIndex(taskIndex, taskCount)) {
+                    taskDone[taskIndex] = true;
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + formatTask(taskDescriptions[taskIndex], taskDone[taskIndex]));
+                }
+            } else if (command.startsWith("unmark ")) {
+                int taskIndex = getTaskIndex(command, "unmark ");
+                if (isValidTaskIndex(taskIndex, taskCount)) {
+                    taskDone[taskIndex] = false;
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + formatTask(taskDescriptions[taskIndex], taskDone[taskIndex]));
                 }
             } else {
-                tasks[taskCount] = command;
+                taskDescriptions[taskCount] = command;
                 taskCount++;
                 System.out.println("added: " + command);
             }
 
             System.out.println(LINE);
         }
+    }
+
+    private static int getTaskIndex(String command, String commandPrefix) {
+        try {
+            return Integer.parseInt(command.substring(commandPrefix.length())) - 1;
+        } catch (NumberFormatException exception) {
+            return -1;
+        }
+    }
+
+    private static boolean isValidTaskIndex(int taskIndex, int taskCount) {
+        return taskIndex >= 0 && taskIndex < taskCount;
+    }
+
+    private static String formatTask(String description, boolean isDone) {
+        String status = isDone ? "[X]" : "[ ]";
+        return status + " " + description;
     }
 }
